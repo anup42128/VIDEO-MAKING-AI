380238c5760337295ed1feeed84bca100769c631
Anup <anuprai42128@gmail.com>
2026-03-23 15:31:13 +0530

    New Changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/VIDEO-MAKING-AI/',
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
